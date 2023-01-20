e7b7077ccc2ea4afbbd907221b57436a04a12957
Beuuuh <bellmarques8768@hotmail.com>
2023-01-20 15:30:33 -0300

    submit button works

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const LOGIN = document.querySelector(".login");
 const ADDBOOK = document.querySelector(".addbook");
+const WARNING = document.querySelector(".warning");
+const POPUP = document.querySelector(".popup");
+const TITLE = document.querySelector(".title");
+const AUTHOR = document.querySelector(".author");
+const PAGES = document.querySelector(".page");
+const SUBMIT = document.querySelector(".submitbtn");
+
+ADDBOOK.addEventListener("click", showPopUp);
+SUBMIT.addEventListener("click", addNewBook);
 
 let library = [];
+let title;
+let author;
+let pages;
+let newBook;
 
 function book(name, author, pages) {
     this.name = name;
@@ -11,4 +24,23 @@ function book(name, author, pages) {
 
 function addToLibrary(book) {
     library.push(book);
+}
+
+function render() {
+    if(library.length == 0) {
+        WARNING.innerHTML = "You have no books!";
+    }
+}
+
+function showPopUp() {
+    POPUP.hidden = false;
+}
+
+function addNewBook(event) {
+    event.preventDefault();
+    title = TITLE.value;
+    author = AUTHOR.value;
+    pages = PAGES.value;
+    let newBook = new book(title, author, pages);
+    addToLibrary(newBook);
 }
